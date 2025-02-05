c158a609048a4f56d311a676d9ca4ca848529c29
ソヌ <sonusid1325@gmail.com>
2025-02-05 22:24:15 +0530

    bottom bar

--- app/src/main/java/com/sonusid/ollama/ui/screens/Home.kt
+++ app/src/main/java/com/sonusid/ollama/ui/screens/Home.kt
@@ -1,18 +1,9 @@
 package com.sonusid.ollama.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -25,38 +16,67 @@ import com.sonusid.ollama.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Home(navHostController: NavHostController){
+fun Home(navHostController: NavHostController) {
+    var userPrompt by remember { mutableStateOf("") }
+
     Scaffold(
         topBar = {
             TopAppBar(
                 title = {
                     Row(
-                        modifier = Modifier.fillMaxWidth().padding(end = 10.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(end = 10.dp),
                         verticalAlignment = Alignment.CenterVertically,
                         horizontalArrangement = Arrangement.SpaceBetween
                     ) {
                         IconButton(onClick = {}) {
-                            Icon(painter = painterResource(R.drawable.logo), contentDescription = "logo", modifier = Modifier.size(30.dp))
+                            Icon(
+                                painter = painterResource(R.drawable.logo),
+                                contentDescription = "logo",
+                                modifier = Modifier.size(30.dp)
+                            )
                         }
                         Text("llama3.2", fontSize = 20.sp)
                         IconButton(onClick = {}) {
-                            Icon(painter = painterResource(R.drawable.settings), contentDescription = "settings", modifier = Modifier.size(30.dp))
+                            Icon(
+                                painter = painterResource(R.drawable.settings),
+                                contentDescription = "settings",
+                                modifier = Modifier.size(30.dp)
+                            )
                         }
                     }
                 }
             )
+        },
+        bottomBar = {
+            OutlinedTextField(
+                value = userPrompt,
+                onValueChange = { userPrompt = it },
+                shape = CircleShape,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .padding(bottom = 20.dp),
+                placeholder = { Text("Enter your prompt...", fontSize = 15.sp) },
+                colors = OutlinedTextFieldDefaults.colors(
+                    unfocusedBorderColor = MaterialTheme.colorScheme.primaryContainer,
+                    focusedBorderColor = MaterialTheme.colorScheme.primaryContainer
+                )
+            )
         }
-    ) {
-        paddingValues ->
+    ) { paddingValues ->
         Column(modifier = Modifier.padding(paddingValues)) {
             Text("Null")
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
-fun HomePreview(){
+fun HomePreview() {
     val dummyHost = rememberNavController()
-    Home(dummyHost)
+    MaterialTheme(colorScheme = darkColorScheme()) {
+        Home(dummyHost)
+    }
 }
