6c6254cc0fbe741b4d42430d82a004c8d9a62820
ソヌ <sonusid1325@gmail.com>
2025-02-05 19:24:49 +0530

    navigation

--- app/src/main/java/com/sonusid/ollama/MainActivity.kt
+++ app/src/main/java/com/sonusid/ollama/MainActivity.kt
@@ -3,34 +3,14 @@ package com.sonusid.ollama
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.sonusid.ollama.db.AppDatabase
-import com.sonusid.ollama.db.repo.ChatRepository
-import com.sonusid.ollama.db.viewmodels.ChatViewModel
-import com.sonusid.ollama.db.viewmodels.ChatViewModelFactory
 import com.sonusid.ollama.ui.theme.OllamaTheme
 
 class MainActivity : ComponentActivity() {
-    private lateinit var viewModel: ChatViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        // Initialize Database & Repository
-        val database = AppDatabase.getDatabase(applicationContext)
-        val repository = ChatRepository(database.userDao())
-
-        // Initialize ViewModel with Factory
-        val factory = ChatViewModelFactory(repository)
-        viewModel = ViewModelProvider(this, factory)[ChatViewModel::class.java]
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -38,7 +18,7 @@ class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
             OllamaTheme {
                 NavHost(navController = navController, startDestination = "home"){
-                    composable("home"){App()}
+                    composable("home"){Home()}
                 }
             }
         }
@@ -49,28 +29,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun Home(){
 
-@Composable
-fun App() {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Greeting(
-            name = "Android",
-            modifier = Modifier.padding(innerPadding)
-        )
-    }
-}
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OllamaTheme {
-        Greeting("Android")
-    }
 }
