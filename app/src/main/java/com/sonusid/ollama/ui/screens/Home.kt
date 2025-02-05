5878789661aeafbcc7c590641f5a29e7ba1b6dd1
ソヌ <sonusid1325@gmail.com>
2025-02-05 21:54:57 +0530

    Top App Bar

--- app/src/main/java/com/sonusid/ollama/ui/screens/Home.kt
+++ app/src/main/java/com/sonusid/ollama/ui/screens/Home.kt
@@ -1,18 +1,27 @@
 package com.sonusid.ollama.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.sonusid.ollama.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,9 +30,17 @@ fun Home(navHostController: NavHostController){
         topBar = {
             TopAppBar(
                 title = {
-                    Row {
+                    Row(
+                        modifier = Modifier.fillMaxWidth().padding(end = 10.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
                         IconButton(onClick = {}) {
-
+                            Icon(painter = painterResource(R.drawable.logo), contentDescription = "logo", modifier = Modifier.size(30.dp))
+                        }
+                        Text("llama3.2", fontSize = 20.sp)
+                        IconButton(onClick = {}) {
+                            Icon(painter = painterResource(R.drawable.settings), contentDescription = "settings", modifier = Modifier.size(30.dp))
                         }
                     }
                 }
