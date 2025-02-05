a3a4a4f01a68d29047d5b427868125e435739bef
ソヌ <sonusid1325@gmail.com>
2025-02-05 22:40:12 +0530

    font

---
+++ app/src/main/java/com/sonusid/ollama/ui/theme/Font.kt
@@ -0,0 +1,11 @@
+package com.sonusid.ollama.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.sonusid.ollama.R
+
+// Define a local font family
+val notoSans = FontFamily(
+    Font(R.font.NotoSans, FontWeight.Normal)
+)
