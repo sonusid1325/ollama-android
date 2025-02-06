15ec0e35d323bbb440cd656c5445281eda27b383
ソヌ <sonusid1325@gmail.com>
2025-02-06 15:05:52 +0530

    settings UI

--- app/src/main/java/com/sonusid/ollama/ui/screens/settings/Settings.kt
+++ app/src/main/java/com/sonusid/ollama/ui/screens/settings/Settings.kt
@@ -4,18 +4,22 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,20 +28,24 @@ import com.sonusid.ollama.R
 @Composable
 fun Settings() {
     var gateway by remember { mutableStateOf("https://localhost:12345") }
+    var valid by remember { mutableStateOf(true) }
     Scaffold(
         topBar = {
             OutlinedTextField(
                 value = gateway,
                 onValueChange = { gateway = it },
-                label = { "Server Address" },
+                placeholder = { Text("https://localhost:12345") },
                 singleLine = true,
-                modifier = Modifier.fillMaxWidth().padding(8.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                shape = CircleShape,
                 colors = OutlinedTextFieldDefaults.colors(
-                    unfocusedBorderColor = MaterialTheme.colorScheme.primaryContainer,
+                    unfocusedBorderColor = if (valid) Color.Green else Color.Red,
                     focusedBorderColor = MaterialTheme.colorScheme.primaryContainer
                 ),
                 suffix = {
-                    IconButton(onClick = {}, modifier = Modifier.size(30.dp)) {
+                    IconButton(onClick = {}, modifier = Modifier.size(25.dp)) {
                         Icon(painter = painterResource(R.drawable.save), contentDescription = "Save Address")
                     }
                 }
@@ -54,5 +62,7 @@ fun Settings() {
 @Preview(showBackground = true)
 @Composable
 fun SettingsPreview() {
-    Settings()
+    MaterialTheme(colorScheme = darkColorScheme()){
+        Settings()
+    }
 }
