5ac06ceaf415958d61efa1ddf24791618ce5043f
ソヌ <sonusid1325@gmail.com>
2025-02-07 14:07:26 +0530

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,11 @@ dependencies {
     // optional - Kotlin Extensions and Coroutines support for Room
     implementation("androidx.room:room-ktx:$room_version")
 
+    // For APIs
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+
     //Generated
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
